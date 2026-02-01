18522ca5e170e29e253615ce883f59089dcc340c
1Md-Rakibul-Islam <rakibul.islam.developer@gmail.com>
2026-02-02 03:50:35 +0600

    feat: project setup and user and product module added

---
+++ app/Models/Product.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'short_description',
+        'description',
+        'price',
+        'sale_price',
+        'stock',
+        'in_stock',
+        'sku',
+        'image',
+        'is_active',
+        'category_id',
+    ];
+
+    protected $casts = [
+        'price' => 'float',
+        'sale_price' => 'float',
+        'in_stock' => 'boolean',
+        'is_active' => 'boolean',
+    ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relationships
+    |--------------------------------------------------------------------------
+    */
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+}
---
+++ database/migrations/2026_02_01_203753_create_products_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            // Basic info
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('short_description')->nullable();
+            $table->longText('description')->nullable();
+
+            // Pricing
+            $table->decimal('price', 10, 2);
+            $table->decimal('sale_price', 10, 2)->nullable();
+
+            // Inventory
+            $table->integer('stock')->default(0);
+            $table->boolean('in_stock')->default(true);
+            $table->string('sku')->nullable();
+
+            // Media
+            $table->string('image')->nullable();
+
+            // Status
+            $table->boolean('is_active')->default(true);
+
+            // Relations (future use)
+            $table->unsignedBigInteger('category_id')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
